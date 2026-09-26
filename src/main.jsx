8aa2ee111caaa1f5791fc5f85bbd78cfb0e09d82
Aditya Pal <adityaj02810@gmail.com>
2026-09-26 13:58:27 +0530

    added-analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AlertProvider } from './Context/AlertContext.jsx'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
